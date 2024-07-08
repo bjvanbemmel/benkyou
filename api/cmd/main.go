7c7e70d43bec282ba05f6d0078e91c550bbfb301
bjvanbemmel <beauvanbemmel@live.nl>
2024-07-09 01:25:37 +0200

    moved main into cmd directory, added postgres service to compose

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
-  fmt.Println("Hello, World!")
-
   r := chi.NewRouter()
+
   r.Use(middleware.Logger)
+  r.Use(middleware.Recoverer)
+  r.Use(middleware.Timeout(time.Second * 60))
+
   r.Get("/", func(w http.ResponseWriter, r *http.Request) {
     w.Write([]byte("Hello, World!"))
   })
