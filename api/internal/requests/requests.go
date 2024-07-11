9b4ed62e4cb30a318620a1f2b87e4218b04d85e3
bjvanbemmel <beauvanbemmel@live.nl>
2024-07-11 22:51:01 +0000

    add validator translations to requests.Validate

--- api/internal/requests/requests.go
+++ api/internal/requests/requests.go
@@ -2,18 +2,53 @@ package requests
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
+	"reflect"
+	"strings"
 
+	"github.com/go-playground/locales/en"
+	ut "github.com/go-playground/universal-translator"
 	"github.com/go-playground/validator/v10"
+	en_translations "github.com/go-playground/validator/v10/translations/en"
 )
+
+var (
+	uni      *ut.UniversalTranslator
+	validate *validator.Validate
+	trans    ut.Translator
+)
+
+func init() {
+	en := en.New()
+	uni = ut.New(en, en)
+
+	trans, _ = uni.GetTranslator("en")
+	validate = validator.New(validator.WithRequiredStructEnabled())
+	en_translations.RegisterDefaultTranslations(validate, trans)
+
+	// Use JSON property names instead of Go struct field names
+	validate.RegisterTagNameFunc(func(fld reflect.StructField) string {
+		name := strings.SplitN(fld.Tag.Get("json"), ",", 2)[0]
+
+		if name == "-" {
+			return ""
+		}
+
+		return name
+	})
+}
 
 type Request interface{}
 
 func Decode[T Request](r *http.Request) (T, error) {
 	var req T
 	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		return req, errors.New("invalid request body")
+	}
 
-	return req, err
+	return req, nil
 }
 
 // Assume that in case of an error, it will always be of type []FieldError.
@@ -25,9 +60,8 @@ func Validate[T Request](r *http.Request) (T, error) {
 		return req, err
 	}
 
-	validate := validator.New(validator.WithRequiredStructEnabled())
 	if err := validate.Struct(req); err != nil {
-		return req, err.(validator.ValidationErrors)[0]
+		return req, errors.New(err.(validator.ValidationErrors)[0].Translate(trans))
 	}
 
 	return req, nil
