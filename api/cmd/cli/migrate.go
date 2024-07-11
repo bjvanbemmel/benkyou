bc1cc6ed0734d952c7734a3943cfe535c01d5f12
bjvanbemmel <beauvanbemmel@live.nl>
2024-07-11 22:25:57 +0000

    remove port env from postsgres connection string for cli migrations

--- api/cmd/cli/migrate.go
+++ api/cmd/cli/migrate.go
@@ -30,9 +30,8 @@ func (c CommandMigrate) Execute(w io.ReadWriter, args ...string) error {
 
 	godotenv.Load("../../../.env")
 	conn := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		"host=%s user=%s password=%s dbname=%s sslmode=disable",
 		os.Getenv("POSTGRES_HOST"),
-		os.Getenv("POSTGRES_PORT"),
 		os.Getenv("POSTGRES_USER"),
 		os.Getenv("POSTGRES_PASSWORD"),
 		os.Getenv("POSTGRES_DB"),
